27c1df310fa473b74c2f5e42f598a7bbcbb188fd
madelei <1913272757@qq.com>
2023-12-01 23:02:44 +0800

    增加 部分 aac解码文件

--- src/decoder/aac_decoder.cc
+++ src/decoder/aac_decoder.cc
@@ -1,3 +1,12 @@
 #include "src/decoder/aac_decoder.h"
 
-namespace _LIB_NAMESPACE::decoder {}
+extern "C" {
+#include "third_party/aac/libAACdec/include/aacdecoder_lib.h"
+}
+
+namespace _LIB_NAMESPACE::decoder {
+AACDecoder::AACDecoder(api::DecodeAudioSink* sink, uint32_t id)
+    : sink_(sink), session_id_(id) {}
+AACDecoder::~AACDecoder() = default;
+
+}  // namespace _LIB_NAMESPACE::decoder
--- src/decoder/aac_decoder.h
+++ src/decoder/aac_decoder.h
@@ -4,8 +4,39 @@
 
 #ifndef SRC_DECODER_AAC_DECODER_H_
 #define SRC_DECODER_AAC_DECODER_H_
+#include "src/api/audio/decode_sink.h"
 #include "src/config/config_site.h"
 
-namespace _LIB_NAMESPACE::decoder {}
+namespace _LIB_NAMESPACE::decoder {
+class AACDecoder {
+ public:
+  // call sink  有可能多路流 区分回调id
+  explicit AACDecoder(api::DecodeAudioSink* sink, uint32_t id);
+
+  AACDecoder(const AACDecoder&) = delete;
+  AACDecoder& operator=(const AACDecoder&) = delete;
+
+  virtual ~AACDecoder();
+
+  bool Initialize(api::AudioFormat* format,
+                  uint32_t bitrate,
+                  uint16_t aot,
+                  uint16_t frame_len);
+  bool DecodeAudio(std::unique_ptr<base::Buffer> buffer);
+
+  void Release();
+
+ private:
+  void Reset();
+  void HandleDecode(std::unique_ptr<base::Buffer> buffer);
+
+ private:
+  uint32_t session_id_;
+
+  api::DecodeAudioSink* sink_;
+  api::AudioFormat audio_format_;
+  bool init_done_ = false;
+};
+}  // namespace _LIB_NAMESPACE::decoder
 
 #endif
