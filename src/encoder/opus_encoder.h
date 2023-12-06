bdcb6248db7e07c426ec606e9e5c43b06486a4de
madelei <1913272757@qq.com>
2023-12-06 18:58:42 +0800

    去掉 无用注释

--- src/encoder/opus_encoder.h
+++ src/encoder/opus_encoder.h
@@ -21,10 +21,6 @@ class OpusEcoder {
 
   virtual ~OpusEcoder() = default;
 
-  // aot lc ld 算法模式  lc aot == 2算法快 低复杂度
-  // AAC-ELD的范围是最广的，包括
-  // 1024，512，480，其次是AAC-LD范围为:512,480,那么对AAC-LC来说就只有1024了。(注意这里的长度是采样点的个数)。
-  // type 0 是TT_MP4_RAW 2是TT_MP4_ADTS
   bool Initialize(base::AudioFormat* format);
   bool EncodeAudio(std::unique_ptr<base::Buffer> buffer);
 
